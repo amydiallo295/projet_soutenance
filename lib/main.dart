05d350b0ebd6f2274e536fe1996cf38bd6dc9032
amydiallo295 <amydiallo295gmail.com>
2024-05-16 10:10:57 +0200

    page d'alerte effectuée

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:emergency/ui/sreens/alertPage.dart';
 import 'package:emergency/ui/sreens/homePage.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: HomePage()
+        home: AlertPage()
 
         // const MyHomePage(title: 'Emergency'),
         );
---
+++ lib/ui/sreens/alertPage.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:badges/badges.dart' as badges;
+
+class AlertPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text(
+            'Emergency',
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: badges.Badge(
+                badgeContent: Text(
+                  '3',
+                  style: TextStyle(color: Colors.white),
+                ),
+                child: Icon(
+                  Icons.notifications,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+/** */
+            IconButton(
+              icon: Icon(Icons.account_circle, color: Colors.white),
+              onPressed: () {
+                // Action lorsque l'icône de profile est cliquée
+              },
+            ),
+          ],
+        ),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue.shade900, Colors.black],
+            ),
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/images/logoApp.png', // Ajoutez le chemin de votre image ici
+                              width:
+                                  200, // Ajustez la largeur selon votre besoin
+                            ),
+                            SizedBox(
+                                height:
+                                    20), // Espace vertical entre l'image et le texte
+                            Text(
+                              "Cliquer sur ce bouton pour emmettre une alerte d'urgence",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                              textAlign: TextAlign.center,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(
+                              10.0), // Augmenter la taille du padding
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Action pour le bouton
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color.fromARGB(
+                                      255, 221, 27, 13), // Couleur du bouton
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(50)
+
+                                  // Forme du bouton en cercle
+                                  ),
+                              child: Text(
+                                'Bouton',
+                                style: TextStyle(
+                                  color: Colors
+                                      .white, // Couleur du texte du bouton
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Color.fromARGB(255, 221, 27, 13), // Couleur du fond
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextButton(
+                onPressed: () {
+                  // Action pour l'accueil
+                },
+                child: Text(
+                  'Accueil',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Action pour les alertes
+                },
+                child: Text(
+                  'Alert',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Action pour les conseils
+                },
+                child: Text(
+                  'Conseil',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
