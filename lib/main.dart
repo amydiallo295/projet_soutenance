eaae3fc20aead326f2f105c48b6679d2883a16c7
Pokpa-max <pokpawolomagui@gmail.com>
2024-05-17 15:30:27 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,64 @@
-import 'package:emergency/ui/sreens/alertPage.dart';
-import 'package:emergency/ui/sreens/emergencyHomePage.dart';
-import 'package:emergency/ui/sreens/homePage.dart';
-import 'package:emergency/ui/sreens/profilePage.dart';
+import 'package:emergency/ui/sreens/home_page/accueil_screen.dart';
+import 'package:emergency/ui/sreens/home_page/profil.dart';
+
 import 'package:flutter/material.dart';
+// ignore: depend_on_referenced_packages
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Emergency',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: HomePage2()
-
-        // const MyHomePage(title: 'Emergency'),
-        );
+      title: 'Bottom Navigation Bar Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
   }
 }
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final selectedIndex = ref.watch(bottomNavigationBarProvider);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Navigation Bar Example'),
+      ),
+      body: IndexedStack(
+        index: selectedIndex,
+        children: [
+          AccueilScreen(),
+          ProfilScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (index) =>
+            ref.read(bottomNavigationBarProvider.notifier).state = index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profil',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+final bottomNavigationBarProvider = StateProvider<int>((ref) => 0);
