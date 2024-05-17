9271df205781820f527e42c301897f6f6788b494
Pokpa-max <pokpawolomagui@gmail.com>
2024-05-17 15:14:29 +0000

    donne

---
+++ lib/ui/sreens/home_page/profil.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// ignore: depend_on_referenced_packages
+class ProfilScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Profil Screen'),
+    );
+  }
+}
