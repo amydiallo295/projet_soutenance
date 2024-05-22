d13bad8579c6b6ddd0e7f4178358557eaf6db924
amydiallo295 <amydiallo295gmail.com>
2024-05-22 13:15:15 +0200

    integration de la fonctionnalite de geolocalisation

--- lib/ui/sreens/reportPage.dart
+++ lib/ui/sreens/reportPage.dart
@@ -1,8 +1,209 @@
+// import 'dart:io';
+// import 'package:flutter/material.dart';
+// import 'package:images_picker/images_picker.dart';
+// import 'package:emergency/utils/app_colors.dart';
+// import 'package:emergency/utils/ui_helpers.dart';
+
+// class EmergencySubmissionPage extends StatefulWidget {
+//   const EmergencySubmissionPage({super.key});
+
+//   @override
+//   _EmergencySubmissionPageState createState() =>
+//       _EmergencySubmissionPageState();
+// }
+
+// class _EmergencySubmissionPageState extends State<EmergencySubmissionPage> {
+//   final TextEditingController nameController = TextEditingController();
+//   final TextEditingController phoneController = TextEditingController();
+//   final TextEditingController descriptionController = TextEditingController();
+//   final TextEditingController locationController = TextEditingController();
+
+//   String? selectedEmergencyType;
+//   final List<String> emergencyTypes = [
+//     'Incendie',
+//     'Accident de la route',
+//     'Crime',
+//     'Urgence médicale',
+//     'Catastrophe naturelle',
+//     'Autre'
+//   ];
+
+//   final List<File> _images = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: primaryColor,
+//         iconTheme: const IconThemeData(color: Colors.white),
+//         title: TitleWidget(text: "ion d'urgence"),
+//       ),
+//       body: SafeArea(
+//         child: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: SingleChildScrollView(
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 const Text(
+//                   'Soumettre une nouvelle urgence',
+//                   style: TextStyle(
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.bold,
+//                     color: appBarColor,
+//                   ),
+//                   textAlign: TextAlign.center,
+//                 ),
+//                 const SizedBox(height: 20),
+//                 TextField(
+//                   controller: nameController,
+//                   decoration: InputDecoration(
+//                     labelText: 'Nom',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 TextField(
+//                   controller: phoneController,
+//                   keyboardType: TextInputType.phone,
+//                   decoration: InputDecoration(
+//                     labelText: 'Numéro de téléphone',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 DropdownButtonFormField<String>(
+//                   value: selectedEmergencyType,
+//                   items: emergencyTypes.map((type) {
+//                     return DropdownMenuItem(
+//                       value: type,
+//                       child: Text(type),
+//                     );
+//                   }).toList(),
+//                   onChanged: (value) {
+//                     setState(() {
+//                       selectedEmergencyType = value;
+//                     });
+//                   },
+//                   decoration: InputDecoration(
+//                     labelText: 'Type d\'urgence',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 TextField(
+//                   controller: descriptionController,
+//                   maxLines: 5,
+//                   decoration: InputDecoration(
+//                     labelText: 'Description',
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(height: 20),
+//                 ElevatedButton(
+//                   onPressed: () async {
+//                     List<Media>? pickedImages = await ImagesPicker.pick(
+//                       count: 3,
+//                       pickType: PickType.image,
+//                     );
+//                     if (pickedImages != null) {
+//                       setState(() {
+//                         _images.addAll(
+//                             pickedImages.map((media) => File(media.path)));
+//                       });
+//                     }
+//                   },
+//                   child: Text('Ajouter des images'),
+//                 ),
+//                 SizedBox(
+//                   height: 200,
+//                   child: ListView.builder(
+//                     scrollDirection: Axis.horizontal,
+//                     itemCount: _images.length,
+//                     itemBuilder: (context, index) {
+//                       return Padding(
+//                         padding: const EdgeInsets.all(8.0),
+//                         child: Image.file(
+//                           _images[index],
+//                           width: 200,
+//                           height: 200,
+//                           fit: BoxFit.cover,
+//                         ),
+//                       );
+//                     },
+//                   ),
+//                 ),
+//                 const SizedBox(height: 30),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     String name = nameController.text;
+//                     String phone = phoneController.text;
+//                     String emergencyType =
+//                         selectedEmergencyType ?? 'Non spécifié';
+//                     String description = descriptionController.text;
+//                     String location = locationController.text;
+
+//                     showDialog(
+//                       context: context,
+//                       builder: (context) {
+//                         return AlertDialog(
+//                           title: const Text('Urgence soumise'),
+//                           content: Text('Nom: $name\n'
+//                               'Numéro de téléphone: $phone\n'
+//                               'Type d\'urgence: $emergencyType\n'
+//                               'Description: $description\n'),
+//                           actions: [
+//                             TextButton(
+//                               onPressed: () {
+//                                 Navigator.of(context).pop();
+//                               },
+//                               child: const Text('OK'),
+//                             ),
+//                           ],
+//                         );
+//                       },
+//                     );
+//                   },
+//                   style: ElevatedButton.styleFrom(
+//                     padding: const EdgeInsets.symmetric(vertical: 15.0),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(12.0),
+//                     ),
+//                     backgroundColor: appBarColor,
+//                     textStyle: const TextStyle(
+//                       fontSize: 18,
+//                       fontWeight: FontWeight.bold,
+//                     ),
+//                   ),
+//                   child: const Text(
+//                     'Soumettre',
+//                     style: TextStyle(color: Colors.white),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:images_picker/images_picker.dart';
 import 'package:emergency/utils/app_colors.dart';
 import 'package:emergency/utils/ui_helpers.dart';
+import "package:images_picker/images_picker.dart";
 
 class EmergencySubmissionPage extends StatefulWidget {
   const EmergencySubmissionPage({super.key});
@@ -110,16 +311,17 @@ class _EmergencySubmissionPageState extends State<EmergencySubmissionPage> {
                 const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: () async {
-                    List<Media>? pickedImages = await ImagesPicker.pick(
-                      count: 3,
-                      pickType: PickType.image,
-                    );
-                    if (pickedImages != null) {
-                      setState(() {
-                        _images.addAll(
-                            pickedImages.map((media) => File(media.path)));
-                      });
-                    }
+                    getImage();
+                    // List<Media>? pickedImages = await ImagesPicker.pick(
+                    //   count: 3,
+                    //   pickType: PickType.image,
+                    // );
+                    // if (pickedImages != null) {
+                    //   setState(() {
+                    //     _images.addAll(
+                    //         pickedImages.map((media) => File(media.path)));
+                    //   });
+                    // }
                   },
                   child: Text('Ajouter des images'),
                 ),
@@ -195,4 +397,17 @@ class _EmergencySubmissionPageState extends State<EmergencySubmissionPage> {
       ),
     );
   }
+
+  
+}
+
+  Future getImage() async {
+    List<Media>? res = await ImagesPicker.pick(
+      count: 3,
+      pickType: PickType.image,
+    );
+// Media
+// .path
+// .thumbPath (path for video thumb)
+// .size (kb)
 }
